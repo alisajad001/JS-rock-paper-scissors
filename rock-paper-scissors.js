3cc0c2df0fe8317f5927c07e34a14c209c2df8eb
Ali Sajad Ahmadi <forwebonly0@gmail.com>
2023-06-04 23:03:53 +0430

    Get computer choice function added

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,3 +8,20 @@ const getUserChoice = (userInput) => {
     console.log('Error! Please select a valid choice');
   }
 }
+
+// 2. Computer choice
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0: 
+        return 'rock';
+      break;
+      case 1: 
+        return 'paper';
+      break;
+      case 2:
+        return 'scissors';
+      break;
+    }
+  }
+  
